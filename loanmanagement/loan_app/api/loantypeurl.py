a46d0e262b14d064c6de1b187750db75a37a037d
sooraz <2dsooraz@gmail.com>
2019-12-08 11:15:02 +0545

    requirements cahnged

--- loanmanagement/loan_app/api/loantypeurl.py
+++
@@ -1,4 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from .views import (
-LoantypelistAPIView,LoantypeDetailAPIView,LoantypeUpdateAPIView,LoantypeDeleteAPIView,LoantypeCreateAPIView)
--- loanmanagement/loan_app/api/paymenturl.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from .views import (
-PaymentlistAPIView,PaymentDetailAPIView,PaymentUpdateAPIView,PaymentDeleteAPIView,PaymentCreateAPIView)
-urlpatterns = [
-
-]
