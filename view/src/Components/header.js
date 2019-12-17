eb6410182668b38519c7d3bbff5e8f59b7f92717
pkdhital <pkdhital@machineertech.com>
2019-12-17 23:40:44 +0545

    Header layout updated

--- view/src/Components/header.js
+++ view/src/Components/header.js
@@ -1,45 +1,73 @@
+// This is the class file for top header of the UI
+/*
+  Top header should contain logo in leftmost side followed by application name
+  On rightmost side there should be UI element indicating if a user is logged in or not
+*/
 import React from 'react';
 class Header extends React.Component{
-  constructor(props){
-    super(props);
-    this.siteName=props.name;
-    this.logo=props.logo;
+  getIdentity= () => {
+    /*
+      MESSAGE TO SOORAZ
+      get path to application logo and name from API
+      I don't think this feature is still built in in backend so just skip this
+      If API exist make a api call and provide value in appropriate variables below
+    */
+    this.logo="https://www.lineacademy.edu.np/media/Partners/MachineerTech.png";
+    this.name="नेपाल आयल निगम कर्जा व्यवस्थापन प्रणाली";
   }
-  Status = (props) =>{
-    if (props.userName ==''){
-      return(
-        <div className="flex-item">
-          Login
-        </div>
-      )
-    }
-    else {
-      return(
-        <div className="flex-item">
-          Welcome, {props.userName}
-        </div>
-      )
-    }
+  getUserName= () =>{
+    /*
+      MESSAGE TO SOORAZ
+      Get userName if user is logged in
+      Set loggedIn variable to true if logged in else set it false.
+    */
+    this.loggedIn=true;
+    this.userName="test user";
+  }
+  logIn= () =>{
+
+  }
+  LogOut= () =>{
+    /*
+      MESSAGE TO SOORAZ
+      make logging out api call and then redirect to login page
+      To redirect to li=ogin page just call this.logIN()
+    */
+    //make api call here
+    this.logIN();
   }
   Identity = () =>{
     return(
-      <div className="flex-item brand-name">
-      <a className="navbar-brand">
-          <img src={this.logo} alt="Logo"/>
-           नेपाल आयल निगम
-      </a>
+      <div className="identity"><img className="logo" width="100px" src={this.logo} alt="Logo"></img><h3 className="app-name">{this.name}</h3></div>
+    )
+  }
+  Status= () =>{
+    if(this.loggedIn){
+      return(
+        <div className="user-detail">
+          <i className="icon-user"></i>
+          <p className="user-name">  Welcome, {this.userName}</p>
+          <button className="logout" onClick={this.logOut}>Logout</button>
+        </div>
+      )
+    }
+    return(
+      <div className="user-detail">
+        <i className="icon-user"></i>
+        <button className="login" onClick={this.logIn}>Login</button>
       </div>
     )
   }
   render(){
-    console.log("here:");
-    console.log(this.siteName);
+    this.getIdentity();
+    this.getUserName();
+    console.log(this.name);
     return(
-      <div className="row row-flex navbar">
-        <this.Identity/>
-        <this.Status/>
+      <div>
+      <this.Identity />
+      <this.Status />
       </div>
     )
-    }
-};
+  }
+}
 export default Header;
